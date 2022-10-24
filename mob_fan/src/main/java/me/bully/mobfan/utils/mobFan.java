d793be59d40c60f5a31abafa488594d26c79975f
Anas8e <94750646+Anas8e@users.noreply.github.com>
2022-10-24 22:19:01 +0100

    MobFan 1.03
    You can now get the fan back when you break it
    Added visual indicator when placing a fan (you can still do /fancheck while targeting a sign to check if it's a mob fan)

--- mob_fan/src/main/java/me/bully/mobfan/utils/mobFan.java
+++ mob_fan/src/main/java/me/bully/mobfan/utils/mobFan.java
@@ -2,17 +2,21 @@ package me.bully.mobfan.utils;
 
 import me.bully.mobfan.MobFan;
 import org.bukkit.ChatColor;
+import org.bukkit.DyeColor;
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
-import org.bukkit.block.BlockState;
-import org.bukkit.block.TileState;
+import org.bukkit.block.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
+
+import java.util.List;
 
 public class mobFan implements Listener {
     //Fan detection and placement if the item in hand is a legit fan
@@ -23,6 +27,12 @@ public class mobFan implements Listener {
         if (e.getItemInHand().getItemMeta().getDisplayName().equals(ChatColor.BLUE + "Mob fan")) {
             if (e.getItemInHand().getItemMeta().getLore().get(0).equals("Mob Fan:")) {
                 if (!(e.getBlock().getFace(e.getBlockAgainst()).equals(BlockFace.UP)) && !(e.getBlock().getFace(e.getBlockAgainst()).equals(BlockFace.DOWN)) && !(e.getBlock().getFace(e.getBlockAgainst()).equals(BlockFace.SELF))) {
+                    Sign sign =(Sign) fan.getState();
+                    sign.setLine(1, "-=[   ]=-");
+                    sign.setLine(2, "MobFan");
+                    sign.setColor(DyeColor.LIGHT_BLUE);
+                    sign.setGlowingText(true);
+                    sign.update();
                     BlockFace face = e.getBlockAgainst().getFace(e.getBlock());
                     BlockState blockState = fan.getState();
                     TileState tileState = (TileState) blockState;
@@ -58,6 +68,25 @@ public class mobFan implements Listener {
                     e.setCancelled(true);
                     placer.sendMessage("Please place the fan on a wall");
                 }
+            }
+        }
+    }
+    @EventHandler
+    public void onFanRemoval(BlockBreakEvent e) {
+        Block block = e.getBlock();
+        Player player = e.getPlayer();
+        if (block.getState() instanceof TileState) {
+            TileState tileState = (TileState) block.getState();
+            PersistentDataContainer container = tileState.getPersistentDataContainer();
+            if (container.has(new NamespacedKey(MobFan.getPlugin(), "fanFace"), PersistentDataType.STRING)) {
+                e.setDropItems(false);
+                ItemStack fan = new ItemStack(Material.OAK_SIGN, 1);
+                ItemMeta fanMeta = fan.getItemMeta();
+                fanMeta.setDisplayName(ChatColor.BLUE + "Mob fan");
+                fanMeta.setLore(List.of("Mob Fan:", "-Pushes entities in the direction", "it's facing.", "Please place the fan on a wall"));
+                fan.setItemMeta(fanMeta);
+                e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), fan);
+                player.sendMessage(ChatColor.RED+"You broke a fan !");
             }
         }
     }
